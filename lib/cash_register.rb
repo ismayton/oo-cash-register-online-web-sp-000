13d7f187ac5f7ad146ffb0cca1718fd842e5b2ee
Ian Mayton <ian.mayton@gmail.com>
2020-07-24 23:29:16 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,9 @@
 class CashRegister 
   attr_accessor :total, :discount, :total, :items
-  @items = []
   
   def initialize(discount = 0)
     @total = 0
+    @items = []
     @discount = discount
   end
   
