af360c09321631d8c44c8a72c6b560d0c73b8b2c
Ian Mayton <ian.mayton@gmail.com>
2020-07-24 23:19:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,4 +1,26 @@
 class CashRegister 
+  attr_accessor :total, :discount, :total, :items
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+  
+  def total 
+    @total 
+  end 
+  
+  def add_item(item, quantity = 1)
+    @items << item
+  end 
+  
+  def apply_discount
+    puts "Success!"
+  end
+
+  def items
+    @items 
+  end
   
 end
 
